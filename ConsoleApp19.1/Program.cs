e615c724ebc6c23c0575a3e835dd1280af5e6247
muxazzXJohny <maracoco334@gmail.com>
2025-02-02 12:35:18 +0200

    task-1

---
+++ ConsoleApp19.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 1;
+            int d = 4;  
+            int n = 5;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = a1 + i * d;
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
