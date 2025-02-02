095f5f00e5a02d423430a357ebd8b6e684a5c106
muxazzXJohny <maracoco334@gmail.com>
2025-02-02 12:41:21 +0200

    Add files via upload

---
+++ ConsoleApp19.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 1; 
+            int d = 4;  
+            int n = 5; 
+            int sum = 0; 
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = a1 + i * d; 
+                Console.Write(num + " ");
+                sum += num; 
+            }
+
+            Console.WriteLine("их cумма: " + sum);
+            Console.ReadLine(); 
+        }
+    }
+}
