887dcc0fdaa147722b05f4e52c3762b3dc3362e9
muxazzXJohny <maracoco334@gmail.com>
2025-02-02 12:38:11 +0200

    task-5-b

---
+++ ConsoleApp19.5.b/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19._5.b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dailyDistance = 10.0;
+            double totalDistance = 0.0;
+            int day = 0;
+
+            while (totalDistance <= 100)
+            {
+                day++;
+                totalDistance += dailyDistance;
+                dailyDistance *= 1.1; 
+            }
+
+            Console.WriteLine($"На {day} день суммарный пробег будет больше 100 км.");
+        }
+    }
+}
