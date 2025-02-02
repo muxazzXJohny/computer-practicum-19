3c31bfe2bdeb3d132e73c5760f7cdc4f0bf82b87
muxazzXJohny <maracoco334@gmail.com>
2025-02-02 12:37:13 +0200

    task-4

---
+++ ConsoleApp17-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("#");
+                Console.ReadKey();
+            }
+        }
+    }
+}
